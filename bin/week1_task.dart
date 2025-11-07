ae9d82923df57cc2c7f840ef6231ea28e351a3e2
Pabitrakhadka517 <pratimakhadka517@gmail.com>
2025-11-07 14:49:14 +0545

    first commit

---
+++ bin/week1_task.dart
@@ -0,0 +1,5 @@
+import 'package:week1_task/week1_task.dart' as week1_task;
+
+void main(List<String> arguments) {
+  print('Hello world: ${week1_task.calculate()}!');
+}
---
+++ test/week1_task_test.dart
@@ -0,0 +1,8 @@
+import 'package:week1_task/week1_task.dart';
+import 'package:test/test.dart';
+
+void main() {
+  test('calculate', () {
+    expect(calculate(), 42);
+  });
+}
